93717d8cc7b9e26c590308236336a97d7fab01b0
JoaoDeMaria <jv.duarte.viana@gmail.com>
2019-01-24 12:08:18 -0200

    Initial commit

---
+++ Scripts/pulpdoctest-script.py
@@ -0,0 +1,12 @@
+#!"c:\users\joão victor\documents\ciência da computação\2º semestre\plin\toolbox_python\scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'PuLP==1.6.9','console_scripts','pulpdoctest'
+__requires__ = 'PuLP==1.6.9'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('PuLP==1.6.9', 'console_scripts', 'pulpdoctest')()
+    )
